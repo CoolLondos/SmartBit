57a17f06bddd304e995d129f55d68e7c7ace2ebc
Londos <londos2021@gmail.com>
2023-02-13 17:10:23 +0000

    MAJOR UPDATE!!!

--- main.ts
+++ main.ts
@@ -1,11 +1,55 @@
+input.onButtonPressed(Button.A, function () {
+    if (App_Selected == "Menu") {
+        Full_Clear()
+        if (App_To_Be_Selected == "Steps") {
+            App_To_Be_Selected = "Compass"
+        }
+    }
+})
 function Full_Clear () {
     led.stopAnimation()
     basic.clearScreen()
 }
+input.onButtonPressed(Button.B, function () {
+    if (App_Selected == "Menu") {
+        Full_Clear()
+        if (App_To_Be_Selected == "Compass") {
+            App_To_Be_Selected = "Steps"
+        } else if (App_To_Be_Selected == "Steps") {
+        	
+        }
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (App_Selected == "Menu") {
+        Full_Clear()
+        App_Selected = App_To_Be_Selected
+        App_To_Be_Selected = "Menu"
+    } else {
+        Full_Clear()
+        App_Selected = "Menu"
+    }
+})
 let Degrees = 0
-let App_To_Be_Selected = "None"
-let App_Selected = "Menu"
+let App_Selected = ""
+let App_To_Be_Selected = ""
+App_To_Be_Selected = "Compass"
+App_Selected = "Menu"
+radio.setTransmitPower(1)
 basic.showString("Select App")
+basic.forever(function () {
+    if (App_Selected == "Menu") {
+        if (App_To_Be_Selected == "Compass") {
+            Full_Clear()
+            basic.showString("Compass")
+        } else if (App_To_Be_Selected == "Steps") {
+            Full_Clear()
+            basic.showString("Steps")
+        } else if (App_To_Be_Selected == "Menu") {
+            App_To_Be_Selected = "Compass"
+        }
+    }
+})
 basic.forever(function () {
     Degrees = input.compassHeading()
     if (App_Selected == "Compass") {
