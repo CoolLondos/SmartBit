4c329fd67b681fe7c23eb78dcd75e1544af88728
Londos <londos2021@gmail.com>
2023-04-22 15:18:04 +0100

    Settings now works!!

--- main.ts
+++ main.ts
@@ -10,10 +10,13 @@ input.onButtonPressed(Button.A, function () {
             App_To_Be_Selected = "Mag Detector"
         }
     } else if (App_Selected == "Settings") {
-        if (Settings_Item == "Sleep") {
-            Settings_Item = "Sleep"
-        } else if (Settings_Item == "Back") {
-            Settings_Item = "Sleep"
+        Full_Clear()
+        if (Settings_Item == "Back") {
+            Settings_Item = "Back"
+        } else if (Settings_Item == "Reboot") {
+            Settings_Item = "Back"
+        } else if (Settings_Item == "Cal compass") {
+            Settings_Item = "Reboot"
         }
     }
 })
@@ -33,10 +36,13 @@ input.onButtonPressed(Button.B, function () {
             App_To_Be_Selected = "Settings"
         }
     } else if (App_Selected == "Settings") {
+        Full_Clear()
         if (Settings_Item == "Back") {
-            Settings_Item = "Back"
-        } else if (Settings_Item == "Sleep") {
-            Settings_Item = "Back"
+            Settings_Item = "Reboot"
+        } else if (Settings_Item == "Reboot") {
+            Settings_Item = "Cal compass"
+        } else if (Settings_Item == "Cal compass") {
+            Settings_Item = "Cal compass"
         }
     }
 })
@@ -50,14 +56,19 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     serial.writeLine("Logo Pressed")
     if (App_Selected == "Menu") {
         Full_Clear()
+        Settings_Item = "Back"
         App_Selected = App_To_Be_Selected
         App_To_Be_Selected = "Menu"
     } else if (App_Selected == "Settings") {
         if (Settings_Item == "Back") {
             Full_Clear()
             App_Selected = "Menu"
-        } else if (Settings_Item == "Sleep") {
+        } else if (Settings_Item == "Reboot") {
             Full_Clear()
+            control.reset()
+        } else if (Settings_Item == "Cal compass") {
+            Full_Clear()
+            input.calibrateCompass()
         }
     } else {
         Full_Clear()
@@ -71,39 +82,10 @@ let App_Selected = ""
 let App_To_Be_Selected = ""
 App_To_Be_Selected = "Compass"
 App_Selected = "Menu"
+Settings_Item = "Back"
 radio.setTransmitPower(1)
 basic.showString("Select App")
 serial.writeString("Serial: " + control.deviceSerialNumber())
-basic.forever(function () {
-    if (App_Selected == "Menu") {
-        if (App_To_Be_Selected == "Compass") {
-            Full_Clear()
-            basic.showString("Compass")
-        } else if (App_To_Be_Selected == "Steps") {
-            Full_Clear()
-            basic.showString("Steps")
-        } else if (App_To_Be_Selected == "Menu") {
-            App_To_Be_Selected = "Compass"
-        } else if (App_To_Be_Selected == "Mag Detector") {
-            Full_Clear()
-            basic.showString("Magnet Detector")
-        } else if (App_To_Be_Selected == "Settings") {
-            Full_Clear()
-            basic.showString("Settings")
-        }
-    }
-})
-basic.forever(function () {
-    if (App_Selected == "Settings") {
-        Full_Clear()
-        if (Settings_Item == "Back") {
-            basic.showString("Back")
-        }
-        if (Settings_Item == "Sleep") {
-            basic.showString("Sleep")
-        }
-    }
-})
 basic.forever(function () {
     if (App_Selected == "Mag Detector") {
         Full_Clear()
@@ -130,6 +112,37 @@ basic.forever(function () {
             basic.showString("W")
         } else {
             basic.showString("N")
+        }
+    }
+})
+basic.forever(function () {
+    if (App_Selected == "Menu") {
+        if (App_To_Be_Selected == "Compass") {
+            Full_Clear()
+            basic.showString("Compass")
+        } else if (App_To_Be_Selected == "Steps") {
+            Full_Clear()
+            basic.showString("Steps")
+        } else if (App_To_Be_Selected == "Menu") {
+            App_To_Be_Selected = "Compass"
+        } else if (App_To_Be_Selected == "Mag Detector") {
+            Full_Clear()
+            basic.showString("Magnet Detector")
+        } else if (App_To_Be_Selected == "Settings") {
+            Full_Clear()
+            basic.showString("Settings")
+        }
+    }
+})
+basic.forever(function () {
+    if (App_Selected == "Settings") {
+        Full_Clear()
+        if (Settings_Item == "Back") {
+            basic.showString("Back")
+        } else if (Settings_Item == "Reboot") {
+            basic.showString("Reboot")
+        } else if (Settings_Item == "Cal compass") {
+            basic.showString("Calibrate compass")
         }
     }
 })
